d4010d0b60c8c80cae95a5e612660961d3651c23
Andrea Calabretta <andreacalabretta0@gmail.com>
2023-01-03 12:15:40 +0100

    38_ Store Service Design - What SUbject to Use?

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject, AsyncSubject, ReplaySubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,25 +13,21 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
-      const subject = new BehaviorSubject(0);
+      const subject = new ReplaySubject();
 
       const series$ = subject.asObservable();
 
-      series$.subscribe( val => console.log("early sub:" + val));
+      series$.subscribe( val => console.log("first sub:" + val));
 
       subject.next(1);
       subject.next(2);
       subject.next(3);
-      // subject.complete();
+      subject.complete();
 
       setTimeout(() => {
-        series$.subscribe( val => console.log("late sub:" + val));
+        series$.subscribe( val => console.log("second sub:" + val));
       }, 3000);
-
-      subject.next(4);
-
     }
-
 }
 
 
