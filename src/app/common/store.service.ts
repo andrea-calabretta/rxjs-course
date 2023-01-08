a1b4e1b50e8b6cc600462c8d83f8c33dc5281c51
Andrea Calabretta <andreacalabretta0@gmail.com>
2023-01-08 12:13:03 +0100

    39. The Store Pattern - Loading Initial Data, Selector Methods, Consuming Data

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { BehaviorSubject, Observable, Subject, timer } from "rxjs";
+import { delayWhen, map, retryWhen, shareReplay, tap } from "rxjs/operators";
 import { Course } from "../model/course";
+import { createHttpObservable } from "./util";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Store {
-
   private subject = new BehaviorSubject<Course[]>([]);
 
   courses$ : Observable<Course[]> = this.subject.asObservable();
+
+  init() {
+    const http$ = createHttpObservable('/api/courses');
+
+    http$
+    .pipe(
+      tap(() => console.log("HTTP request executed")),
+      map(res => Object.values(res["payload"]) ),
+      )
+      .subscribe(
+        courses => this.subject.next(courses)
+        );
+      }
+
+
+  selectBeginnerCourses(): Observable<Course[]> {
+    return this.filterByCategory('BEGINNER');
+  }
+
+  selectAdvancedCourses(): Observable<Course[]> {
+    return this.filterByCategory('ADVANCED');
+  }
+
+  filterByCategory(category: string): Observable<Course[]> {
+    return this.courses$
+        .pipe(
+          map(courses => courses
+              .filter(course => course.category == category))
+        )
+  }
 }
